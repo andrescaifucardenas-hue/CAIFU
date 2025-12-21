eabdbf9b4b92865d9c350965a18e749366bba36d
andrescaifucardenas-hue <andrescaifucardenas@gmail.com>
2025-12-20 22:28:44 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const CACHE_NAME = 'caifu-v850';
-const urlsToCache = [
-  './',
-  './index.html',
-  'https://raw.githubusercontent.com/andrescaifucardenas-hue/CAIFU/main/logo.png.png'
-];
+// Cambia la primera línea a v890
+const CACHE_NAME = 'caifu-v890'; 
+const urlsToCache = [ './', './index.html' ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  self.skipWaiting(); // Esto obliga a la nueva versión a activarse
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => {
+    if(k !== CACHE_NAME) return caches.delete(k);
+  }))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
