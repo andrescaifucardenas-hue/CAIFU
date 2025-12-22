7a57a8842be9a3f57ec3503aaf0217d2d99f4b06
andrescaifucardenas-hue <andrescaifucardenas@gmail.com>
2025-12-22 18:14:40 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia la primera línea a v900
-const CACHE_NAME = 'caifu-v950-ios';
+const CACHE_NAME = 'caifu-v960-horario';
 const urlsToCache = [ './', './index.html' ];
 
 self.addEventListener('install', e => {
